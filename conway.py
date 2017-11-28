1918203bbcd792774b3da1a88b647b6fd5838a0b
AsbjornOlling <asbjornolling@gmail.com>
2017-11-28 21:56:08 +0100

    ability to do single step forward

--- conway.py
+++ conway.py
@@ -1,92 +1,50 @@
+# This file contains the main loop of the program, and all the pygame stuff.
+# This is the main file to be executed
+
 import pygame
 from pygame.locals import *
+import settings
+from cell import Cell
+
+
+def draw(cell):
+    if cell.populated:
+        color = settings.black
+    else:
+        color = settings.white
+    pygame.draw.rect(gameDisplay, color, (cell.x * settings.cell_width, cell.y * settings.cell_height, settings.cell_width, settings.cell_height))
+
+# simulation step one forward
+def simulation_step():
+        # iterate through grid, finding cells to flip
+        cells_to_flip = []
+        for x in range(0, settings.grid_width):
+            for y in range(0, settings.grid_height):
+                if grid[x][y].check_for_flip():
+                    cells_to_flip.append((x,y))
+
+        # flip all cells at the same time
+        for cell_coords in cells_to_flip:
+            grid[cell_coords[0]][cell_coords[1]].flip()
+            draw(grid[cell_coords[0]][cell_coords[1]])
+
 
 pygame.init()
 
-# set resolution
-display_width = 1800
-display_height = 1000
-
-# define grid
-grid_width = 180  # dimensions in cells
-grid_height = 100
-cell_width = display_width / grid_width  # cell dimensions in pixels
-cell_height = display_height / grid_height
-grid = []
-
-# colors
-black = (0,0,0)
-white = (255,255,255)
-
 # define window
-gameDisplay = pygame.display.set_mode((display_width, display_height)) 
+gameDisplay = pygame.display.set_mode((settings.display_width, settings.display_height)) 
 pygame.display.set_caption("CONWAY")
-gameDisplay.fill(white)
+gameDisplay.fill(settings.white)
 
-# behavior for the cells
-class Cell:
-    def __init__(self, coord):
-        self.x = coord[0]
-        self.y = coord[1]
-        self.populated = False
-
-        # make list of neighbor coords to check
-        self.neighbor_coords = []
-        for xdiff in range(-1,2):
-            check_x = self.x + xdiff
-            for ydiff in range(-1,2):
-                check_y = self.y + ydiff
-                # dont check cells outside grid
-                if 0 <= check_x < grid_width - 1 and 0 <= check_y < grid_height - 1:
-                    # cell shouldn't count itself
-                    if not (check_x == self.x and check_y == self.y):
-                        self.neighbor_coords.append((check_x,check_y))
-
-
-    # counts number of neighbors
-    # and decides whether to flip the tile
-    def check_for_flip(self):
-        # count populated neighbors
-        populated_neighbors = 0
-        for coord in self.neighbor_coords:
-            if grid[coord[0]][coord[1]].populated:
-                populated_neighbors += 1
-
-        # then act on the result
-        # for populated cells
-        if self.populated:
-            # cells with 0-1 neighbors die of solitude
-            if populated_neighbors < 2:
-                return True
-            # cells with > 4 die from overpoupulation
-            elif populated_neighbors > 4:
-                return True
-        # empty cells with three neighbors become populated
-        elif populated_neighbors == 3:
-            return True
-
-
-    def flip(self):
-        if self.populated:
-            self.populated = False
-        else:
-            self.populated = True
-
-
-    def draw(self):
-        if self.populated:
-            color = black
-        else:
-            color = white
-        pygame.draw.rect(gameDisplay, color, (self.x * cell_width, self.y * cell_height, cell_width, cell_height))
-
-## end class
+# object that handles timing
+clock = pygame.time.Clock()                      
 
 
 # fill the grid with cell objects
-for x in range(0, grid_width):
+grid = []
+for x in range(0, settings.grid_width):
     column = []
-    for y in range(0, grid_height):
+    for y in range(0, settings.grid_height):
         column.append(Cell( (x,y) ))
     grid.append(column)
 
@@ -96,15 +54,12 @@ grid[3][2].populated = True
 grid[1][3].populated = True
 grid[2][3].populated = True
 grid[3][3].populated = True
-grid[2][1].draw()
-grid[3][2].draw()
-grid[1][3].draw()
-grid[2][3].draw()
-grid[3][3].draw()
+draw(grid[2][1])
+draw(grid[3][2])
+draw(grid[1][3])
+draw(grid[2][3])
+draw(grid[3][3])
 pygame.display.update()
-
-# object that handles timing
-clock = pygame.time.Clock()                      
 
 # start main loop
 crashed = False
@@ -113,29 +68,16 @@ while not crashed:
 
     # main event handler
     for event in pygame.event.get():
-
-        # exit event
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # quit
             crashed = True
         elif event.type == KEYDOWN:
-            # space toggles pause
-            if event.key == K_SPACE:
-                print("space GOT")
+            if event.key == K_SPACE: # toggle pause
                 paused = not paused
-
-    # simulation step one forward
+            elif event.key == K_RIGHT:
+                simulation_step()
+    
     if not paused:
-        # iterate through grid, finding cells to flip
-        cells_to_flip = []
-        for x in range(0, grid_width):
-            for y in range(0,grid_height):
-                if grid[x][y].check_for_flip():
-                    cells_to_flip.append((x,y))
-
-        # flip all cells at the same time
-        for cell_coords in cells_to_flip:
-            grid[ cell_coords[0] ][ cell_coords[1] ].flip()
-            grid[ cell_coords[0] ][ cell_coords[1] ].draw()
+        simulation_step()
 
     # update entire screen
     pygame.display.update()
