076d7e43aa30ed9e7fcb014bc399bba3841030a0
AsbjornOlling <asbjornolling@gmail.com>
2017-11-28 21:46:54 +0100

    switched to multi-module structure

---
+++ cell.py
@@ -0,0 +1,48 @@
+import settings
+
+class Cell:
+    def __init__(self, coord):
+        self.x = coord[0]
+        self.y = coord[1]
+        self.populated = False
+
+        # make list of neighbor coords to check
+        self.neighbor_coords = []
+        for xdiff in range(-1,2):
+            check_x = self.x + xdiff
+            for ydiff in range(-1,2):
+                check_y = self.y + ydiff
+                # dont check cells outside grid
+                if 0 <= check_x < settings.grid_width - 1 and 0 <= check_y < settings.grid_height - 1:
+                    # cell shouldn't count itself
+                    if not (check_x == self.x and check_y == self.y):
+                        self.neighbor_coords.append((check_x,check_y))
+
+
+    # counts number of neighbors
+    # and decides whether to flip the tile
+    def check_for_flip(self):
+        # count populated neighbors
+        from conway import grid
+        populated_neighbors = 0
+        for coord in self.neighbor_coords:
+            if grid[coord[0]][coord[1]].populated:
+                populated_neighbors += 1
+
+        # then act on the result
+        # for populated cells
+        if self.populated:
+            # cells with 0-1 neighbors die of solitude
+            if populated_neighbors < 2:
+                return True
+            # cells with > 4 die from overpoupulation
+            elif populated_neighbors > 4:
+                return True
+        # empty cells with three neighbors become populated
+        elif populated_neighbors == 3:
+            return True
+
+
+    # inverts the color
+    def flip(self):
+        self.populated = not self.populated
---
+++ settings.py
@@ -0,0 +1,13 @@
+# set window size in pixels
+display_width = 1800
+display_height = 1000
+
+# define grid
+grid_width = 180  # grid dimensions in cells
+grid_height = 100
+cell_width = display_width / grid_width  # cell dimensions in pixels
+cell_height = display_height / grid_height
+
+# colors
+black = (0,0,0)
+white = (255,255,255)
